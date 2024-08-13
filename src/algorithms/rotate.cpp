fa1c4cef2b1b59648f89d9d6a3d1aace748d3cb0
Nastasia Kovářová <xkovarov@fi.muni.cz>
2024-08-13 11:06:50 +0200

    fix rotate 180

--- src/algorithms/rotate.cpp
+++ src/algorithms/rotate.cpp
@@ -32,7 +32,7 @@ void rotate_r(Image& img, QString&) {
 void rotate_180(Image& img, QString&) {
     std::size_t cols = img.size().width;
     std::size_t rows = img.size().height;
-    Image new_image(Size(rows, cols), img.type());
+    Image new_image(Size(cols, rows), img.type());
     for (std::size_t col = 0; col < cols; ++col) {
         for (std::size_t row = 0; row < rows; ++row) {
             new_image.setPixel(Coords(col, row), img.pixel(Coords(cols - col - 1, rows - row - 1)));
