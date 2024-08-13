81ce966ff811dd33ce05eeacb465c46ba97787b2
Nastasia Kovářová <xkovarov@fi.muni.cz>
2024-08-13 18:12:24 +0200

    implement histogram create

--- src/algorithms/histogram.cpp
+++ src/algorithms/histogram.cpp
@@ -1,9 +1,29 @@
 #include "algorithms/histogram.hpp"
-
+#include "gui/main_bar.hpp"
 #include "core/image.hpp"
+#include <array>
 
 namespace algorithms {
-void histogram_create(Image&, QString&) {}
+void histogram_create(Image& img, QString& name) { 
+	std::array<std::size_t, 256> hist = {0};
+    for (std::size_t col = 0; col < img.size().width; ++col) {
+        for (std::size_t row = 0; row < img.size().height; ++row) {
+            auto pixel = img.pixel<uint8_t>(Coords(col, row));
+            ++hist[pixel];
+        }
+    }
+    std::size_t max = *std::ranges::max_element(hist);
+    auto histogram = Image(Size(256, max), ImageType::G8);
+    histogram.fillWith(uint8_t(0));
+    for (std::size_t h = 0; h < 256; ++h) {
+        for (std::size_t i = 0; i < hist[h]; ++i) {
+            histogram.setPixel(Coords(h, max - i - 1), uint8_t(255));
+        }
+    }
+    MainBarSingleton::getInstance()->openImage(histogram, name + "_histogram");
+}
 
 void histogram_normalize(Image&, QString&) {}
+
+void histogram_match(Image&, QString&) {}
 } // namespace algorithms
--- src/algorithms/histogram.hpp
+++ src/algorithms/histogram.hpp
@@ -5,4 +5,5 @@
 namespace algorithms {
 void histogram_create(Image& img, QString& name);
 void histogram_normalize(Image& img, QString& name);
+void histogram_match(Image& img, QString& name);
 } // namespace algorithms
