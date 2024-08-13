385c8d7da786814665a4b859290ecdf8069caa41
Nastasia Kovářová <xkovarov@fi.muni.cz>
2024-08-13 16:48:21 +0200

    implrmrnt linear stretch

--- src/algorithms/contrast.cpp
+++ src/algorithms/contrast.cpp
@@ -13,6 +13,19 @@ void contrast(Image& img, double by) {
         }
     }
 }
+
+std::pair<uint8_t, uint8_t> minmax(Image& img) {
+    uint8_t min = 255;
+    uint8_t max = 0;
+    for (std::size_t col = 0; col < img.size().width; ++col) {
+        for (std::size_t row = 0; row < img.size().height; ++row) {
+            auto pixel = img.pixel<uint8_t>(Coords(col, row));
+            min = std::min(min, pixel);
+            max = std::max(max, pixel);
+        }
+    }
+    return std::pair(min, max);
+}
 } // namespace details
 
 void contrast_add_50(Image& img, QString&) { details::contrast(img, 1.5); }
@@ -29,5 +42,14 @@ void contrast_by_custom(Image& img, QString&) {
     }
 }
 
-void linear_stretch(Image&, QString&) {}
+void linear_stretch(Image& img, QString&) { 
+    auto [min, max] = details::minmax(img);
+    double factor = 255.0 / (max - min);
+    for (std::size_t col = 0; col < img.size().width; ++col) {
+        for (std::size_t row = 0; row < img.size().height; ++row) {
+            auto pixel = img.pixel<uint8_t>(Coords(col, row));
+            img.setPixel(Coords(col, row), uint8_t(std::lround(double(pixel - min) * factor)));
+        }
+    }
+}
 } // namespace algorithms
