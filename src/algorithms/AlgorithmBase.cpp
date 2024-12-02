52958a26bb051ab45c514e34a33027f8148f48f3
Jan Juračka <jan.juracka@tescan.com>
2024-12-02 11:40:12 +0100

    fix compiler warnings

--- src/algorithms/AlgorithmBase.cpp
+++ src/algorithms/AlgorithmBase.cpp
@@ -2,6 +2,8 @@
 
 #include <cassert>
 
+#define MAYBE_UNUSED(x) (void)x
+
 namespace algorithms {
 
 std::size_t SimpleAlgorithmBase::partCount() const /* final */ { return 1; }
@@ -9,30 +11,35 @@ std::size_t SimpleAlgorithmBase::partCount() const /* final */ { return 1; }
 QString SimpleAlgorithmBase::getName(std::size_t part) const /* final */ {
     assert(partCount() == 1);
     assert(part == 0);
+    MAYBE_UNUSED(part);
     return getName();
 }
 
 bool SimpleAlgorithmBase::isHighlighted(std::size_t part) const /* final */ {
     assert(partCount() == 1);
     assert(part == 0);
+    MAYBE_UNUSED(part);
     return false;
 }
 
 bool SimpleAlgorithmBase::isEnabled(std::size_t part) const /* final */ {
     assert(partCount() == 1);
     assert(part == 0);
+    MAYBE_UNUSED(part);
     return true;
 }
 
 bool SimpleAlgorithmBase::isTypeSupported(std::size_t part, image::ImageType type) const /* final */ {
     assert(partCount() == 1);
     assert(part == 0);
+    MAYBE_UNUSED(part);
     return isTypeSupported(type);
 }
 
 void SimpleAlgorithmBase::apply(std::size_t part, QString& name, image::Image& image) /* final */ {
     assert(partCount() == 1);
     assert(part == 0);
+    MAYBE_UNUSED(part);
     return apply(name, image);
 }
 
